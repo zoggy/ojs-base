0984eaad836fe5dac3dad12a788e28acf1877b6d
Zoggy <zoggy@bat8.org>
2014-10-22 09:23:15 +0200

    fix required versions of libs

--- checkocaml.ml
+++ checkocaml.ml
@@ -899,7 +899,7 @@ let _ = !print "\n### checking required tools and libraries ###\n"
 
 
 let _ = check_ocamlfind_package conf ~min_version: [2;4;1] "js_of_ocaml";;
-let _ = check_ocamlfind_package conf ~fail: false ~min_version: [0;9] "websocket"
+let _ = check_ocamlfind_package conf ~fail: false ~min_version: [0;8;1] "websocket"
 let _ = check_ocamlfind_package conf ~fail: false ~min_version: [2;4;6] "lwt.unix"
 let _ = check_ocamlfind_package conf ~fail: false ~min_version: [1;1;8] "yojson"
 let _ = check_ocamlfind_package conf ~fail: false ~min_version: [1;1] "ppx_deriving_yojson"
